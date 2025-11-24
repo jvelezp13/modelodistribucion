500932785597474f5c224f179184cccdb63fd21a
jvelezp13 <jvelez.nexo@gmail.com>
2025-11-24 17:39:06 -0500

    fix: rename migration to 0003 to resolve conflict

--- admin_panel/core/migrations/0003_add_logistic_profile_types.py
+++ admin_panel/core/migrations/0003_add_logistic_profile_types.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),  # Adjust this to your latest migration
+        ('core', '0002_configuraciondescuentos_tramodescuentofactura'),
     ]
 
     operations = [
